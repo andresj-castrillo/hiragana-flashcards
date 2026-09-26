8e78f917c086100e601e3577d28ca0839a9b2aa8
andresj-castrillo <ancastrillo@javeriana.edu.co>
2026-09-26 01:22:52 -0500

    feat: create visual identity for app and light mode theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// apps visual identity
+// simple color scheme
+// sakura / pink
+abstract final class AppTheme {
+  static const Color seedColor = Color(0xFFB33951); 
+
+  static ThemeData light() {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: seedColor,
+        brightness: Brightness.light,
+      ),
+      appBarTheme: const AppBarTheme(centerTitle: true),
+    );
+  }
+
+  
+}
